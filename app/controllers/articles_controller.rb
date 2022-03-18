ee60bd7bd709337d90fc3d86e7f465dc144e6d8c
Bilal Hussain <bilalhussian785@gmail.com>
2022-03-18 17:30:15 +0500

    adding associations between users and articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,8 +10,9 @@ class ArticlesController < ApplicationController
   end
 
   def create
+    debugger
     @article = Article.new(article_params)
-
+    # @article.user = User.first
     if @article.save
       flash[:notice] = "Article is successfully created"
       redirect_to articles_path
